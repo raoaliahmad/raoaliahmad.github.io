af3cf445b4f4475d15e617e12e3c9461429f1252
ali ahmad <raoaliahmad03@gmail.com>
2021-02-06 20:48:47 +0500

    upgraded to react

---
+++ src/pages/App.js
@@ -0,0 +1,44 @@
+
+function App() {
+  return (
+	  <div className="App">
+		<div class="card">
+				<h1>I'm Ali Ahmad.</h1>
+				<p>
+					A passionate Full Stack Web Developer,
+				</p>
+			</div>
+			<div class="card">
+				<h3>My Skills</h3>
+				<ul>
+					<li>React</li>
+					<li>Elixir</li>
+					<li>Css</li>
+					<li>Typescript</li>
+				</ul>
+			</div>
+			<div class="card">
+				<h3>Have Worked On</h3>
+				<ul>
+					<li><a href="https://Mischool.pk">Mischool</a></li>
+					<li><a href="https://ilmexchange.com">ilmExchange</a></li>
+				</ul>
+			</div>
+			<div class="card">
+				<h3>Important Links:</h3>
+				<ul>
+					<li><a href="https://github.com/raoaliahmad">Github</a></li>
+					<li><a href="https://www.linkedin.com/in/rao-ali-ahmad-a25829130">LinkedIn</a></li>
+				</ul>
+			</div>
+			<div class="card">
+				<h3>Contact Me</h3>
+				<ul>
+					<li><b>Email: </b>raoaliahmad03@gmail.com</li>
+				</ul>
+			</div>
+    </div>
+  );
+}
+
+export default App;
